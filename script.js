1b5c66ed4f4f08705e72fe4f7012bf2ec45e9754
austinev <bgotchall@gmail.com>
2019-11-15 16:35:15 -0600

    working out querty syntax

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ var api_key = "6WXJHRCFYQFNXRVSBL";
 // search with a zipcode: 
 //  this works: https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
 //search with a date range:
-//startDateTime=2019-11-14
+//startDateTime=2019-11-15T01:00:00Z
 //endDateTime=2019-11-24  //q={"status": "GOLD"}
-//https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-14&endDateTime=2019-11-24&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
+//https://app.ticketmaster.com/discovery/v2/events.json?q={"postalCode":"78702"}&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
 var TM_api_key="3JcNn4ea56JrBolF27QIGsWgd58v9GSZ";
+//this works, searching zipcode and keyword:
+//queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&keyword=surface&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
+//this works for location and dates:
+//'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-15T01:00:00Z&endDateTime=2019-12-30T01:00:00Z&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
 
 
-queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-14&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
+queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-15T01:00:00Z&endDateTime=2019-12-30T01:00:00Z&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
     $.ajax({
         url: queryURL,
         method: "GET"
