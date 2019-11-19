9a7559512464eca420bddf3a097b840ef19acc43
austinev <bgotchall@gmail.com>
2019-11-18 20:36:04 -0600

    basically works with table

--- script.js
+++ script.js
@@ -25,32 +25,39 @@ var TM_api_key = "3JcNn4ea56JrBolF27QIGsWgd58v9GSZ";
 //console.log(queryURL);
 var location;
 
- //add in some default values so I don't go crazy: (get rid of this for release)////////
- 
- $("#location").val("Denver");
- console.log($("#start_date"));
- $("#start_date").val("2019-11-16");
- $("#end_date").val("2019-12-20");
- //////////////////////////////////
+//add in some default values so I don't go crazy: (get rid of this for release)////////
+
+//to do list for bob:  
+//1) make the date and time nice
+//2) add the "buy tickets" link
+//3) choose the best picture to use based on the resolution
+
+
+$("#location").val("Austin");
+console.log($("#start_date"));
+$("#start_date").val("2019-11-16");
+$("#end_date").val("2019-12-20");
+//////////////////////////////////
 
 $("#btn_submit").on("click", function (event) {
     event.preventDefault;
-    debugger;
-    var start_date=convert_date($("#start_date")[0].value);
-    var end_date=convert_date($("#end_date")[0].value);
+    //debugger;
+    var start_date = convert_date($("#start_date")[0].value);
+    var end_date = convert_date($("#end_date")[0].value);
     console.log($("#search_field")[0]);
-    var keyword=$("#search_field")[0].value;
-    var location=$("#location")[0].value;
-    console.log("location read is: "+location);
+    var keyword = $("#search_field")[0].value;
+    var location = $("#location")[0].value;
+    console.log("location read is: " + location);
 
-   
+
+
 
     //queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&keyword='+keyword+'&startDateTime='+start_date+'&endDateTime='+end_date+'&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
-    queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?&locale=*&city='+location+'&keyword='+keyword+'&startDateTime='+start_date+'&endDateTime='+end_date+'&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
+    queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?&locale=*&city=' + location + '&keyword=' + keyword + '&startDateTime=' + start_date + '&endDateTime=' + end_date + '&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
 
     console.log(queryURL);
 
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -62,44 +69,51 @@ $("#btn_submit").on("click", function (event) {
         var new_td;
         var new_th;
         var clean_date;
+        var timeZone;
         console.log(response);
         console.log($("#start_date"));
         console.log(event_array);
         draw_header();   //make a new header:
-       
-        for (var i = 0; i < event_array.length; i++) {
-            
 
+        for (var i = 0; i < event_array.length; i++) {
             new_table_row = $("<tr>");
+
+            ////////Event name///////////////////
             new_td = $("<td></td>");
             $(new_td).text(event_array[i].name);
             $(new_table_row).append(new_td);
-           
+
+            /////////date ("start date")//////////////
             new_td = $("<td></td>");
-            clean_date = event_array[i].dates.start.dateTime.split("T")[0];
-            $(new_td).text(clean_date);
-            $(new_table_row).append(new_td);
-            
-            new_td = $("<td></td>");
-            //check if there is an end date.  some things dont have any
-            clean_date = event_array[i].classifications[0].segment.name;
-            console.log ("segment: "+clean_date);
-            if (clean_date!=undefined){
-            // clean_date = event_array[i].dates.end.dateTime.split("T")[0];  //have to make sure it is present in the object or this command throws an error
-            } else { clean_date=" ";}
+            timeZone = event_array[i].dates.timezone;
+            //clean_date = event_array[i].dates.start.dateTime.split("T")[0];
+            var my_thing = get_date_and_time(event_array[i].dates.start.dateTime, timeZone);
             $(new_td).text(clean_date);
             $(new_table_row).append(new_td);
 
+            // new_td = $("<td></td>");
+            // //check if there is an end date.  some things dont have any
+            // clean_date = event_array[i].classifications[0].segment.name;
+            // console.log("segment: " + clean_date);
+            // if (clean_date != undefined) {
+                
+            //     // clean_date = event_array[i].dates.end.dateTime.split("T")[0];  //have to make sure it is present in the object or this command throws an error
+            // } else { clean_date = " "; }
+            // $(new_td).text(clean_date);
+            // $(new_table_row).append(new_td);
 
+            /////////venue/////////////
             new_td = $("<td></td>");
             $(new_td).text(event_array[i]._embedded.venues[0].name);
             $(new_table_row).append(new_td);
 
-            // console.log(event_array[i].images[1].url);
-            // new_td = $("<td><img src=" + event_array[i].images[1].url + " height=" + 150 + " width=" + 300 + "></td>");
-            // // var new_img=$('<img src='+event_array[i].images[3].url+'>');
-            // // $(new_td).innerHTML(new_img);        //empty for now, not sure this makes sense
-            // $(new_table_row).append(new_td);
+            //////////picture////////////
+            console.log(event_array[i].images[1].url);
+            //iterate through the images to find a big hi res one.
+            new_td = $("<td><img src=" + event_array[i].images[1].url + " height=" + 150 + " width=" + 300 + "></td>");
+            // var new_img=$('<img src='+event_array[i].images[3].url+'>');
+            // $(new_td).innerHTML(new_img);        //empty for now, not sure this makes sense
+            $(new_table_row).append(new_td);
 
             $("#event_results").append(new_table_row);
         }
@@ -110,17 +124,52 @@ $("#btn_submit").on("click", function (event) {
 
 })
 
-function convert_date (in_date){
-//convert a date from an input date field into the format that the API wants:
-// example output: 2020-08-01T14:00:00Z
-// example input: 2019-11-15
-    return in_date+"T14:00:00Z";        //the time doesn't matter for this.
+function convert_date(in_date) {
+    //convert a date from an input date field into the format that the API wants:
+    // example output: 2020-08-01T14:00:00Z
+    // example input: 2019-11-15
+    return in_date + "T14:00:00Z";        //the time doesn't matter for this.
 }
 
-function get_date_and_time(in_date){
+function get_date_and_time(in_date, timeZone) {
     //here I am getting a time like this: 2019-12-29T02:30:00Z
     //and I need to output this: 8:30 pm Dec 29, 2019 
     //I need to convert for timezone... the api returns the timezone
+var time_delta;
+var date_first_part;
+var date_second_part;
+var year;
+var month;
+var day;
+//debugger;
+date_first_part = in_date.split("T")[0];
+date_second_part=in_date.split("T")[1];
+
+console.log(in_date);
+console.log(date_first_part);
+console.log(in_date);
+    switch (timeZone) {
+        case "America/New_York":
+            time_delta=-5;
+            break;
+        case "America/Chicago":
+            time_delta=-6;
+            break;
+        case "America/Denver":
+            time_delta=-7;
+            break;
+        case "America/Los_Angeles":
+            time_delta=-8;
+            break;
+        case "Pacific/Honolulu":
+            time_delta=-10;
+            break;
+        default:
+            break;
+    }
+
+
+
     //America/Chicago
     //America/New_York
     //America/Denver
@@ -128,21 +177,21 @@ function get_date_and_time(in_date){
     //Pacific/Honolulu
 }
 
-function draw_header(){
+function draw_header() {
     new_table_row = $("<tr>");
-        new_th = $("<th></th>");
-        $(new_th).text("Event Name");
-        $(new_table_row).append(new_th);
-        new_th = $("<th></th>");
-        $(new_th).text("Start");
-        $(new_table_row).append(new_th);
-        new_th = $("<th></th>");
-        $(new_th).text("End");
-        $(new_table_row).append(new_th);
-        new_th = $("<th></th>");
-        $(new_th).text("Location");
-        $(new_table_row).append(new_th);
-        $("#event_results").append(new_table_row);
+    new_th = $("<th></th>");
+    $(new_th).text("Event Name");
+    $(new_table_row).append(new_th);
+    new_th = $("<th></th>");
+    $(new_th).text("Start");
+    $(new_table_row).append(new_th);
+    new_th = $("<th></th>");
+    $(new_th).text("End");
+    $(new_table_row).append(new_th);
+    new_th = $("<th></th>");
+    $(new_th).text("Location");
+    $(new_table_row).append(new_th);
+    $("#event_results").append(new_table_row);
 }
 
 
