87708faf8978cd2ff05500ba445316f1621ac6b8
austinev <bgotchall@gmail.com>
2019-11-20 19:47:07 -0600

    cleaned comments

--- script.js
+++ script.js
@@ -7,31 +7,15 @@
 var api_key = "6WXJHRCFYQFNXRVSBL";
 
 //ticketmaster stuff:
-//https://app.ticketmaster.com/discovery/v2/events.json?apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
-// search with a zipcode: 
-//  this works: https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
-//search with a date range:
-//startDateTime=2019-11-15T01:00:00Z
-//endDateTime=2019-11-24  
-//https://app.ticketmaster.com/discovery/v2/events.json?q={"postalCode":"78702"}&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ
 var TM_api_key = "3JcNn4ea56JrBolF27QIGsWgd58v9GSZ";
-//this works, searching zipcode and keyword:
-//queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&keyword=surface&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
-//this works for location and dates:
-//'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-15T01:00:00Z&endDateTime=2019-12-30T01:00:00Z&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
-//640x360 looks good
-
-//queryURL = 'https://app.ticketmaster.com/discovery/v2/events.json?postalCode=78702&startDateTime=2019-11-15T01:00:00Z&endDateTime=2019-12-30T01:00:00Z&apikey=3JcNn4ea56JrBolF27QIGsWgd58v9GSZ';
-//console.log(queryURL);
 var location;
 
-//add in some default values so I don't go crazy: (get rid of this for release)////////
-
+//////add in some default values so I don't go crazy: (get rid of this for release)////////
 //to do list for bob:  
 //1) make the date and time nice
 //2) add the "buy tickets" link
 //3) choose the best picture to use based on the resolution
-
+////////////////////////////////////////////////////////////////
 
 $("#location").val("Austin");
 console.log($("#start_date"));
